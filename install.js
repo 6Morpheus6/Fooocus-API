d12f2c630f4e15d919de8257081389743650c87d
6Morpheus6 <wanton-decay@gmx.net>
2025-06-09 20:33:27 +0200

    share models

--- install.js
+++ install.js
@@ -31,5 +31,38 @@ module.exports = {
         ]
       }
     },
+    {
+      method: "fs.link",
+      params: {
+        drive: {
+          checkpoints: "app/repositories/Fooocus/models/checkpoints",
+          clip: "app/repositories/Fooocus/models/clip",
+          clip_vision: "app/repositories/Fooocus/models/clip_vision",
+          configs: "app/repositories/Fooocus/models/configs",
+          controlnet: "app/repositories/Fooocus/models/controlnet",
+          diffusers: "app/repositories/Fooocus/models/diffusers",
+          embeddings: "app/repositories/Fooocus/models/embeddings",
+          gligen: "app/repositories/Fooocus/models/gligen",
+          hypernetworks: "app/repositories/Fooocus/models/hypernetworks",
+          inpaint: "app/repositories/Fooocus/models/inpaint",
+          loras: "app/repositories/Fooocus/models/loras",
+          prompt_expansion: "app/repositories/Fooocus/models/prompt_expansion",
+          style_models: "app/repositories/Fooocus/models/style_models",
+          unet: "app/repositories/Fooocus/models/unet",
+          upscale_models: "app/repositories/Fooocus/models/upscale_models",
+          vae: "app/repositories/Fooocus/models/vae",
+          vae_approx: "app/repositories/Fooocus/models/vae_approx"
+        },
+        peers: [
+          "https://github.com/cocktailpeanut/fluxgym.git",
+          "https://github.com/cocktailpeanutlabs/automatic1111.git",
+          "https://github.com/cocktailpeanutlabs/comfyui.git",
+          "https://github.com/cocktailpeanutlabs/fooocus.git",
+          "https://github.com/pinokiofactory/comfy.git",
+          "https://github.com/pinokiofactory/MagicQuill.git",
+          "https://github.com/pinokiofactory/stable-diffusion-webui-forge.git"
+        ]
+      }
+    }
   ]
 }
