7d5fe95446853c0e4e4ff99eb0c0488754cb42a6
6Morpheus6 <wanton-decay@gmx.net>
2025-06-09 18:00:47 +0200

    add flags

--- pinokio.js
+++ pinokio.js
@@ -1,10 +1,29 @@
 const path = require('path')
 module.exports = {
   version: "3.7",
-  title: "Fooocus-API.git",
-  description: "",
+  title: "Fooocus-API",
+  description: "Fooocus powered by FastAPI",
   icon: "icon.jpeg",
   menu: async (kernel, info) => {
+    // exception handling for windows amd
+    let windowsAmd = (kernel.gpu === "amd" && kernel.platform === "win32")
+    let extraFlags = (windowsAmd ? " --directml --disable-in-browser" : " --disable-in-browser")
+
+    // windows AMD => directml
+    let windowsAMD = (kernel.platform === "win32" && kernel.gpu === "amd")
+    let directml = (windowsAMD ? "--directml " : "")
+    console.log({ windowsAMD, directml })
+
+    let accelerated
+
+    if (kernel.platform === "darwin") {
+      accelerated = kernel.arch === "arm64" // m1/m2/m3
+    } else {
+      accelerated = ["nvidia", "amd"].includes(kernel.gpu)  // gpu is amd/nvidia
+    }
+
+    let alwaysCPU = (accelerated ? "" : "--always-cpu ")
+
     let installed = info.exists("app/env")
     let running = {
       install: info.running("install.js"),
@@ -33,6 +52,7 @@ module.exports = {
             icon: 'fa-solid fa-terminal',
             text: "Terminal",
             href: "start.js",
+            params: { flags: `${alwaysCPU}${directml}--preset default${extraFlags}` }
           }]
         } else {
           return [{
@@ -40,6 +60,7 @@ module.exports = {
             icon: 'fa-solid fa-terminal',
             text: "Terminal",
             href: "start.js",
+            params: { flags: `${alwaysCPU}${directml}--preset default${extraFlags}` }
           }]
         }
       } else if (running.update) {
@@ -69,6 +90,7 @@ module.exports = {
           icon: "fa-solid fa-power-off",
           text: "Start",
           href: "start.js",
+          params: { flags: `${alwaysCPU}${directml}--preset default${extraFlags}` }
         }, {
           icon: "fa-solid fa-plug",
           text: "Update",
